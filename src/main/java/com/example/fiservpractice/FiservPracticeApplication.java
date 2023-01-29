6c00625008eac4ccd50639373e6bea9d10714b1b
arpitp8595 <arpitp8595@gmail.com>
2023-01-28 19:54:34 -0500

    Removing unUsed imports

--- src/main/java/com/example/fiservpractice/FiservPracticeApplication.java
+++ src/main/java/com/example/fiservpractice/FiservPracticeApplication.java
@@ -1,9 +1,7 @@
 package com.example.fiservpractice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class FiservPracticeApplication {
